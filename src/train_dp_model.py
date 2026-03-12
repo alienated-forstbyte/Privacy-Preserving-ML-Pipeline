78e1406c5fd17355ea4d7231e27bea35c528a893
tagrawal3 <tagrawal@aistrike.com>
2026-03-13 00:56:43 +0530

    updated train_dp_model

--- src/train_dp_model.py
+++ src/train_dp_model.py
@@ -70,6 +70,14 @@ with mlflow.start_run():
     mlflow.log_param("batch_size", 32)
     mlflow.log_param("noise_multiplier", 1.1)
 
+    # Build model
+    model = tf.keras.Sequential([
+        tf.keras.layers.Input(shape=(X_train.shape[1],)),
+        tf.keras.layers.Dense(64, activation="relu"),
+        tf.keras.layers.Dense(32, activation="relu"),
+        tf.keras.layers.Dense(1, activation="sigmoid")
+    ])
+
     history = model.fit(
         X_train,
         y_train,
